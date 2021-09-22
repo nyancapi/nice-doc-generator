fc79f8821d5584c382a2774f3cff0d27b7de0b83
tomo-taku <tmrtkm1227tksx@gmail.com>
2021-09-22 17:00:40 +0900

    [update] エラーハンドリングを変更

--- src/libs/convertor.ts
+++ src/libs/convertor.ts
@@ -30,16 +30,7 @@ const tranlate = async (text: string, targetLang: string) => {
       return res.data.translations[0].text;
     })
     .catch((error) => {
-      if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log("Error", error.message);
-      }
-      console.log(error.config);
+      console.log(error);
     });
 
   return result;
