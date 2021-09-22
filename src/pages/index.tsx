bbe81cadf90961d3663f3b10cf935d184b573364
tomo-taku <tmrtkm1227tksx@gmail.com>
2021-09-22 13:17:07 +0900

    [update] レスポンシブ対応

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,41 @@
 const Home = () => {
   return (
     <div className={"relative min-h-screen text-gray-600 flex flex-col"}>
-      <main className={"px-16"}>
+      <main className={"px-8 lg:px-16"}>
         <div className={"text-center mt-12"}>
           <h1 className={"text-2xl"}>
-            あなたの文章を<span className={"dot-text"}>い</span>
+            あなたの文章を
+            <span className={"dot-text"}>い</span>
             <span className={"dot-text"}>い</span>
             <span className={"dot-text"}>感</span>
             <span className={"dot-text"}>じ</span>に変換します
           </h1>
         </div>
-        <div className={"mt-16 flex flex-row justify-center space-x-16"}>
-          <div className={"w-1/2"}>
+        <div
+          className={
+            "mt-16 flex flex-col justify-center space-x-0 space-y-16 lg:space-x-16 lg:space-y-0 lg:flex-row"
+          }
+        >
+          <div className={"lg:w-1/2"}>
             <h2 className={"text-2xl text-center font-semibold"}>Before</h2>
-            <div className={"mt-8 p-12 text-box"}>
+            <div className={"mt-4 p-6 text-box lg:p-12 lg:mt-8"}>
               <textarea
                 placeholder="テキストを入力してください"
                 className={
-                  "w-full h-36 text-lg border-2 border-gray-200 focus:border-gray-300"
+                  "w-full h-36 rounded text-lg border-2 border-gray-200 focus:border-gray-300"
                 }
               ></textarea>
             </div>
           </div>
-          <div className={"w-1/2"}>
+          <div className={"lg:w-1/2"}>
             <h2 className={"text-2xl text-center font-semibold"}>After</h2>
-            <div className={"mt-8 p-12 text-box"}>
+            <div className={"mt-4 p-6 text-box lg:p-12 lg:mt-8"}>
               <textarea
                 placeholder="変換後のテキストはここに表示されます"
                 readOnly={true}
-                className={"w-full h-36 text-lg border-2 border-gray-200"}
+                className={
+                  "w-full h-36 rounded text-lg border-2 border-gray-200"
+                }
               ></textarea>
             </div>
           </div>
